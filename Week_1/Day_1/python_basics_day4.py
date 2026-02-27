faaf7bf4b7d760d99b15c1f588f9f8758ad5950c
Preetham S U <preetham.umesh2003@gmail.com>
2026-02-27 11:28:43 +0100

    Initial commit

---
+++ Week_1/Day_1/python_basics_day4.py
@@ -0,0 +1,16 @@
+#day4:program 1 sum of elements in the list
+num=[10,20,30,40]
+total=0
+for i in num:
+    total+=i
+print("The total is:",total)
+
+#day4:program 2 remove unique elements
+
+nums=[1,1,1,2,2,3,3,4,4,5,6,7]
+unique_num=set(nums)
+print(unique_num)
+
+#day4:program 3 frequency of elements
+
+
